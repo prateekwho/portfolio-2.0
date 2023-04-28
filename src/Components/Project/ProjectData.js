12b0a3e4b91a4ab30611889f1f7513c38bdc0cc5
Prateek <prateekkukreja@outlook.com>
2023-04-28 08:37:27 +0530

    updated project's stack

--- src/Components/Project/ProjectData.js
+++ src/Components/Project/ProjectData.js
@@ -6,7 +6,7 @@ export const items = [
     id: 1,
     name: "E-commerce Website",
     des: "My React and Tailwind CSS e-commerce website offers a modern, responsive design and a user-friendly interface, making online shopping a convenient and enjoyable experience.",
-    stack: ["React", "Tjailwind CSS"],
+    stack: ["React", "Tailwind CSS"],
     img: Ecommerce,
     links: {
       github: "https://github.com/prateekwho/react-ecommerce",
